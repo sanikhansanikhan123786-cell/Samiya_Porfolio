6a781e7b76a65e52d150836936793b2dc548d4f9
Samiya <samiya.bibi@deltashoppe.com>
2026-04-13 11:14:23 +0500

    Update contact information in PortfolioService and contact component

--- src/app/core/services/portfolio.service.ts
+++ src/app/core/services/portfolio.service.ts
@@ -5,7 +5,7 @@ export class PortfolioService {
   readonly name = 'Samiya Bibi';
   readonly role = 'Senior Software Engineer (Frontend)';
   readonly location = 'Rawalpindi, Pakistan';
-  readonly email = 'samyakhan244@gmail.com';
-  readonly phone = '+92 345 516 0030';
+  readonly email = 'samyakhan245@gmail.com';
+  readonly phone = '+92 345 516 0036';
   readonly linkedinUrl = 'https://www.linkedin.com/in/samiya-raja-b771ab138/';
 }
